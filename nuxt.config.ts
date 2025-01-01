772a7dd9a63e48aef03ce2f1b1ba4120f071c1f8
Yury Kravchenko <yury.kravchenko.1987@gmail.com>
2025-01-02 01:07:19 +0300

    Add

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/content'],
+  modules: ['@nuxt/content', '@nuxthq/studio'],
+
   routeRules: {
     '/': { prerender: true }
-  }
+  },
+
+  compatibilityDate: '2025-01-02'
 })
